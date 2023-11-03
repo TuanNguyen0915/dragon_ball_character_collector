0abc26222072ee408873f980da5495b53f97ceeb
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-03 15:03:18 -0400

    add homepage and create model Character

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+# characters = [
+#     Character(
+#         "Goku",
+#         "Kamehameha",
+#         "Goku's signature attack. A powerful Ki blast fired with two hands after concentrating a large amount of Ki.",
+#     ),
+#     Character(
+#         "Vegeta",
+#         "Galick Gun",
+#         "Vegeta's signature technique. Vegeta curls his fingers and places both his hands together at chest level facing the same direction (so that the palm of one hand is on the back of the other).",
+#     ),
+#     Character(
+#         "Frieza",
+#         "Dealth Beam",
+#         "To perform the technique, the user extends his right arm and fires a small, thin, very fast and concentrated laser-like beam of ki from his index finger, which barrels down and pierces through the opponent.",
+#     ),
+#     Character(
+#         "Frieza",
+#         "Dealth Beam",
+#         "To perform the technique, the user extends his right arm and fires a small, thin, very fast and concentrated laser-like beam of ki from his index finger, which barrels down and pierces through the opponent.",
+#     ),
+# ]
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    skill = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,41 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Character:
-    def __init__(self, name, skill, description):
-        self.name = name
-        self.skill = skill
-        self.description = description
-
-
-characters = [
-    Character(
-        "Goku",
-        "Kamehameha",
-        "Goku's signature attack. A powerful Ki blast fired with two hands after concentrating a large amount of Ki.",
-    ),
-    Character(
-        "Vegeta",
-        "Galick Gun",
-        "Vegeta's signature technique. Vegeta curls his fingers and places both his hands together at chest level facing the same direction (so that the palm of one hand is on the back of the other).",
-    ),
-    Character(
-        "Frieza",
-        "Dealth Beam",
-        "To perform the technique, the user extends his right arm and fires a small, thin, very fast and concentrated laser-like beam of ki from his index finger, which barrels down and pierces through the opponent.",
-    ),
-    Character(
-        "Frieza",
-        "Dealth Beam",
-        "To perform the technique, the user extends his right arm and fires a small, thin, very fast and concentrated laser-like beam of ki from his index finger, which barrels down and pierces through the opponent.",
-    ),
-]
+from .models import Character
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>This is the homepage</h1>")
+    return render(request, "home.html")
 
 
 def about(request):
@@ -43,4 +12,5 @@ def about(request):
 
 
 def character_index(request):
-    return render(request, "characters/index.html", {"chars": characters})
+    chars = Character.objects.all()
+    return render(request, "characters/index.html", {"chars": chars})
